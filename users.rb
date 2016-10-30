bd5864b1d1e4c92bd14fac272e57fd84f85f162a
margify <davidd@agilit.co.uk>
2016-10-30 09:54:27 +0000

    Create users.rb
    A ruby script using octokit to find users associated with a given keyword.

---
+++ users.rb
@@ -0,0 +1,23 @@
+require 'octokit'
+
+subject, output = ARGV
+
+client = Octokit::Client.new :access_token => ENV['YOUR TOKEN HERE']
+results = client.search_users(subject)
+total_count = results.total_count
+last_response = client.last_response
+number_of_pages = last_response.rels[:last].href.match(/page=(\d+).*$/)[1]
+
+file = open(output, 'w')
+puts "There are #{total_count} results, on #{number_of_pages} pages!"
+items = results.items
+until last_response.rels[:next].nil?
+  #items.each { |i| file.write( "#{i.login}, #{i.followers_url}\n" ) }
+  items.each { |i|
+    file.write ( "[\n" )
+    i.each { |j| file.write ( "#{j}\n" ) }
+    file.write ( "]\n" )
+  last_response = last_response.rels[:next].get
+  items = last_response.data.items
+end
+file.close
